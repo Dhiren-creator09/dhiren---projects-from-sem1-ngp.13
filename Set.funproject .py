25a07a6f034ed5f3c8dd2856ecc5290574a63ce4
Dhiren-creator09 <dhirenkumar9908@gmail.com>
2026-01-22 11:48:07 +0530

    Create Set.funproject .py

---
+++ Set.funproject .py
@@ -0,0 +1,9 @@
+set1 = set()
+print(type(set1))
+set3 = {"dhiren kumar", 67, "independent", "red", "custom"}
+
+set5 = {"trip to hawaii", 540000, "free tissue paper", 908888, "house appliance"}
+new = set5.pop()
+a = float(input("enter your numbers:"))
+print("chance towin a lucky draw/ enetr you're favourite numbers and see if the fate is with you!!!")
+print(new)
