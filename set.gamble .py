5b87232f6c2dde2b964db14a70fdad0c1600254b
Dhiren-creator09 <dhirenkumar9908@gmail.com>
2026-01-27 21:16:31 +0530

    Update set.gamble .py

--- set.gamble .py
+++ set.gamble .py
@@ -1,9 +1,16 @@
+
+import random
+
 set1 = set()
 print(type(set1))
+
 set3 = {"dhiren kumar", 67, "independent", "red", "custom"}
 
 set5 = {"trip to hawaii", 540000, "free tissue paper", 908888, "house appliance"}
-new = set5.pop()
+
 a = float(input("enter your numbers:"))
-print("chance towin a lucky draw/ enetr you're favourite numbers and see if the fate is with you!!!")
+print("chance to win a lucky draw / enter your favourite numbers and see if fate is with you!!!")
+
+new = random.choice(list(set5))
 print(new)
+
